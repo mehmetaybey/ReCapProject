9a4fef2b8a008e0c40443ca6882f193da964c757
mehmetaybey <53934055+mehmetaybey@users.noreply.github.com>
2021-02-04 19:42:14 +0300

    Delete dbo.Tab3le.sql

--- dbo.Tab3le.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE Car(
-	id INT PRIMARY KEY IDENTITY(1,1),
-	[CarId] VARCHAR(50) NOT NULL,
-	[BrandId] INT NOT NULL,
-	
-	[ModelYear] DATETIMEOFFSET NOT NULL,
-	[DailyPrice] MONEY NOT NULL, 
-    [Description] TEXT NOT NULL
-);
