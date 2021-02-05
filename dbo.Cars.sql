35e4a20eb39c7fe540d47199a9d56e43b4fc9f5f
mehmetaybey <53934055+mehmetaybey@users.noreply.github.com>
2021-02-05 14:04:17 +0300

    Delete dbo.Cars.sql

--- dbo.Cars.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Cars] (
-    [CarId]       INT             NOT NULL,
-    [ColorId]     INT             NOT NULL,
-    [BrandId]     INT             NOT NULL,
-    [ModelYear]   DATE NOT NULL,
-    [DailyPrice]  DECIMAL (18)    NOT NULL,
-    [Description] NVARCHAR (MAX)  NOT NULL
-);
-
